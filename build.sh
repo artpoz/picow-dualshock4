9bd00fb828e629102dff5394b09a187031528fdb
Akatsuki1910 <35941346+Akatsuki1910@users.noreply.github.com>
2024-06-23 22:06:01 +0900

    a

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 git submodule update --init --recursive
+sed -i -e "s/#define MAX_ATTRIBUTE_VALUE_SIZE 300/#define MAX_ATTRIBUTE_VALUE_SIZE 512/" ./btstack/src/classic/hid_host.c
 mkdir build
 cd build
-cmake -DPICO_BOARD=pico_w -DPICO_SDK_PATH=../pico-sdk .
+cmake -DPICO_BOARD=pico_w -DPICO_SDK_PATH=../pico-sdk ../
 make
